286d306a11a4cfb46614120f1872a9813d11d8df
Penguin_Spy <me@penguinspy.dev>
2023-08-24 17:54:33 -0700

    use track length to set remaining score when track played

--- generate/tracks.py
+++ generate/tracks.py
@@ -8,8 +8,19 @@ tracks_cfg = JsonFile(source_path="generate/tracks.json").data
 def beet_default(ctx: Context):
   lang = {}
   sounds = {}
-  for path, title in tracks_cfg.items():
+  longest_track = {
+    "length": -1
+  }
+  for path, (title, length_str) in tracks_cfg.items():
     track_id = path.split("/")[-1]  # just use the filename of the path as the track_id
+    mins, secs = length_str.split(":")
+    length = (int(mins) * 60) + int(secs) + 10  # wait at least 10 seconds after each track
+    if length > longest_track["length"] and not track_id == "end" and not track_id == "credits":
+      longest_track = {
+        "id": track_id,
+        "length": length,
+        "length_str": length_str
+      }
 
     # {"translate": "music_sync.track.ID"}
     lang[f"music_sync.track.{track_id}"] = title
@@ -25,8 +36,11 @@ def beet_default(ctx: Context):
     # function music_sync:track/ID
     ctx.data.functions[f"music_sync:track/{track_id}"] = Function([
       f'playsound music_sync:track.{track_id} music @s',
-      'title @s actionbar {"translate":"record.nowPlaying","with":[{"translate":"music_sync.track.' + track_id + '"}],"color":"light_purple"}'
+      'title @s actionbar {"translate":"record.nowPlaying","with":[{"translate":"music_sync.track.' + track_id + '"}],"color":"light_purple"}',
+      f'scoreboard players set @s music_sync.remaining {length}'
     ])
 
   ctx.assets["music_sync:en_us"] = Language(lang)
   ctx.assets["music_sync"].sound_config = SoundConfig(sounds)
+
+  print("longest track: " + longest_track["id"] + " (" + longest_track["length_str"] +")")
--- src/data/music_sync/functions/second.mcfunction
+++ src/data/music_sync/functions/second.mcfunction
@@ -5,8 +5,8 @@ scoreboard players remove @a[scores={music_sync.remaining=1..}] music_sync.remai
 scoreboard players reset @a[scores={music_sync.leave_game=1..}] music_sync.remaining
 scoreboard players reset @a[scores={music_sync.leave_game=1..}] music_sync.leave_game
 
-# start a new track every 5 minutes (TODO: sync this to the day/night cycle)
+# start a new track every 10 minutes (TODO: sync this to the day/night cycle)
 scoreboard players add $timer music_sync.track 1
-execute if score $timer music_sync.track matches 300.. run function music_sync:start_music
+execute if score $timer music_sync.track matches 600.. run function music_sync:start_music
 
 schedule function music_sync:second 1s
