70f8a00b758ce8cd412789dab2d0f9d2df9db23a
Penguin_Spy <me@penguinspy.dev>
2023-08-27 14:56:13 -0700

    reorganize some code in events.py

--- generate/events.py
+++ generate/events.py
@@ -15,11 +15,8 @@ event_conditions = events_cfg["conditions"]
 
 DEFAULT_EVENT = "music.game"
 
-# takes a list of sound event definitions (`name:"path/to/sound"`) and converts it to a list of track_ids
-# handles event references and weights (but not both in the same event)
-def sounds_to_tracks(event_id, all_events):
-  sounds = all_events[event_id]["sounds"]
-
+# takes a list of sound event definitions (`name:"path/to/sound"`) and converts it to a list of tracks, plus the total weight of all the sounds
+def sounds_to_tracks(sounds):
   total_weight = 0
   tracks = []
   for sound in sounds:
@@ -35,41 +32,27 @@ def sounds_to_tracks(event_id, all_events):
 
   return tracks, total_weight
 
-# creates the event function and returns the condition for it to play
-def create_event(ctx, event_id, condition, all_events):
-  tracks, total_weight = sounds_to_tracks(event_id, all_events)
-
-  if len(tracks) == 0:
-    print(f"[info] skipping trigger for '{event_id}' because it contains 0 tracks")
-    return
-
-  # only generate function if one doesn't already exist
-  if f"music_sync:event/{event_id}" in ctx.data.functions.keys():
-    print(f"[info] using existing function for event/{event_id}")
+# creates the event function (music_sync:event/ID)
+def create_event(ctx, event_id, tracks, total_weight):
+  cmds = [
+    "tag @s add music_sync.played_track"
+  ]
+  if len(tracks) > 1: # only generate a randomizer if there's more than one track
+    cmds.extend([
+      f"scoreboard players set $max music_sync.track {total_weight}",
+      f"execute unless score {event_id} music_sync.track matches -1.. summon minecraft:marker run function music_sync:get_random",
+      f"execute unless score {event_id} music_sync.track matches -1.. run scoreboard players operation {event_id} music_sync.track = $random music_sync.track"
+    ])
+    weight_used = 0 # keep track of how much of the total weight has been used
+    for track in tracks:
+      weight = track['weight']
+      weight_str = f"{weight_used}" if weight == 1 else f"{weight_used}..{weight_used + weight - 1}"
+      weight_used += weight
+      cmds.append(f"execute if score {event_id} music_sync.track matches {weight_str} run function music_sync:{track['type']}/{track['name']}")
   else:
-    # function music_sync:event/ID
-    cmds = [
-      "tag @s add music_sync.played_track"
-    ]
-    if len(tracks) > 1: # only generate a randomizer if there's more than one track
-      cmds.extend([
-        f"scoreboard players set $max music_sync.track {total_weight}",
-        f"execute unless score {event_id} music_sync.track matches -1.. summon minecraft:marker run function music_sync:get_random",
-        f"execute unless score {event_id} music_sync.track matches -1.. run scoreboard players operation {event_id} music_sync.track = $random music_sync.track"
-      ])
-      weight_used = 0 # keep track of how much of the total weight has been used
-      for track in tracks:
-        weight = track['weight']
-        weight_str = f"{weight_used}" if weight == 1 else f"{weight_used}..{weight_used + weight - 1}"
-        weight_used += weight
-        cmds.append(f"execute if score {event_id} music_sync.track matches {weight_str} run function music_sync:{track['type']}/{track['name']}")
-    else:
-      cmds.append(f"function music_sync:{tracks[0]['type']}/{tracks[0]['name']}")
+    cmds.append(f"function music_sync:{tracks[0]['type']}/{tracks[0]['name']}")
 
-    ctx.data.functions[f"music_sync:event/{event_id}"] = Function(cmds)
-
-  # add line to music_sync:player_start_music
-  return f"execute unless entity @s[tag=music_sync.played_track] if {condition} run function music_sync:event/{event_id}"
+  ctx.data.functions[f"music_sync:event/{event_id}"] = Function(cmds)
 
 
 order = events_cfg["order"]
@@ -109,28 +92,44 @@ def beet_default(ctx: Context):
 
   # generate a tag & condition for each music event
   for event_id, biomes in event_trigger_biomes.items():
-    if not event_id in event_conditions.keys():
+    if event_id not in event_conditions.keys():
       if len(biomes) > 1: # generate tag & condition that uses the tag
         ctx.data[f"music_sync:{event_id}"] = WorldgenBiomeTag({
           "values": [f"minecraft:{biome}" for biome in biomes]
         })
         event_conditions[event_id] = f"biome ~ ~ ~ #music_sync:{event_id}"
-      elif len(biomes) > 0: # just directly check the biome
+      else: # just directly check the biome
         event_conditions[event_id] = f"biome ~ ~ ~ minecraft:{biomes[0]}"
-      else:
-        print(f"[warn] no biomes trigger event '{event_id}' & no trigger specified. event will not play")
     else:
       if len(biomes) > 0:
         print(f"[warn] using specified condition for '{event_id}' ('{event_conditions[event_id]}') even though '{biomes}' trigger it")
 
+  # validate no events were left behind
+  for event_id in vanilla_sounds.keys():
+    if not event_id.startswith("music."): continue
+    if event_id in events_cfg["exclude_events"]: continue
+    if event_id not in event_conditions.keys():
+      print(f"[warn] no biomes trigger event '{event_id}' & no trigger specified. event will not play")
 
   # parse each event to create the event function & add its condition to music_sync:player_start_music
   start_music_cmds = [
     "tag @s remove music_sync.played_track"
   ]
   for event_id in sorted(event_conditions, key=sort_by_cfg):
-    cmd = create_event(ctx, event_id, event_conditions[event_id], vanilla_sounds)
-    if cmd:
-      start_music_cmds.append(cmd)
+    sounds = vanilla_sounds[event_id]["sounds"]
+    tracks, total_weight = sounds_to_tracks(sounds)
+
+    if len(tracks) == 0:
+      print(f"[info] skipping trigger for '{event_id}' because it contains 0 tracks")
+      continue
+
+    # only generate function if one doesn't already exist
+    if f"music_sync:event/{event_id}" in ctx.data.functions.keys():
+      print(f"[info] using existing function for event/{event_id}")
+    else:
+      create_event(ctx, event_id, tracks, total_weight)
+
+    # add line to music_sync:player_start_music
+    start_music_cmds.append(f"execute unless entity @s[tag=music_sync.played_track] if {event_conditions[event_id]} run function music_sync:event/{event_id}")
 
   ctx.data.functions[f"music_sync:player_start_music"] = Function(start_music_cmds)
